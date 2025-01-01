d421b6b467732b9d4c9426cfd4d22623b1135419
Furina <51257784+xorsheng@users.noreply.github.com>
2025-01-01 13:50:01 +0800

    refactor: remove unused localization files and components in Chinese language pack

--- src/locales/lang/zh_CN/pages/index.ts
+++ src/locales/lang/zh_CN/pages/index.ts
@@ -1,41 +1,13 @@
-import common from './common';
 import dashboardBase from './dashboard-base';
 import dashboardDetail from './dashboard-detail';
-import detailBase from './detail-base';
-import detailCard from './detail-card';
-import detailDeploy from './detail-deploy';
-import detailSecondary from './detail-secondary';
-import formBase from './form-base';
-import formStep from './form-step';
-import listBase from './list-base';
-import listCard from './list-card';
-import listFilter from './list-filter';
-import listTree from './list-tree';
 import login from './login';
-import monitor from './monitor';
 import result from './result';
-import system from './system';
-import client from './system/client';
 import user from './user';
 
 export default {
   dashboardBase,
   dashboardDetail,
-  listBase,
-  listCard,
-  listFilter,
-  listTree,
-  detailBase,
-  detailCard,
-  detailDeploy,
-  detailSecondary,
-  formBase,
-  formStep,
   user,
   login,
   result,
-  common,
-  system,
-  monitor,
-  client,
 };
--- src/locales/lang/zh_CN/pages/monitor/index.ts
+++
@@ -1,7 +0,0 @@
-import logininfo from './logininfo';
-import operlog from './operlog';
-
-export default {
-  operlog,
-  logininfo,
-};
